6f38a21ce50f0731f367b883eeb5e5e528ecfa3f
Jack McMillan <jackmcmillan1249@icloud.com>
2021-05-27 11:27:22 -0500

    Second Commit

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,9 +1,16 @@
 const navSlide = () => {
   const optionsSelector = document.querySelector('.options-selector');
   const nav = document.querySelector('.links');
+  const navLinks = document.querySelectorAll('.links li');
 
+  //Toggling the nav
   optionsSelector.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+  });
+
+  //animating the links
+  navLinks.forEach((link, index) => {
+    link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 +0.3}s';
   });
 }
 
