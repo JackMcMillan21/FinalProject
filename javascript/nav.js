142a131de1feab15339a0ae5239fa51ea421f3c6
Jack McMillan <jackmcmillan1249@icloud.com>
2021-05-27 23:03:57 -0500

    Burger icon aint working

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,17 +1,24 @@
 const navSlide = () => {
-  const optionsSelector = document.querySelector('.options-selector');
-  const nav = document.querySelector('.links');
-  const navLinks = document.querySelectorAll('.links li');
+  const optionsSelector = document.querySelector(".options-selector");
+  const nav = document.querySelector(".links");
+  const navLinks = document.querySelectorAll(".links li");
 
-  //Toggling the nav
-  optionsSelector.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-  });
+  optionsSelector.addEventListener("click", () => {
+    //Toggling the nav
+    nav.classList.toggle("nav-active");
 
-  //animating the links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    //animating the links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    //options selector annimation
+    optionsSelector.classList.toggle('toggle');
+
   });
-}
+};
 
 navSlide();
