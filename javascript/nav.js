d595ad4b84385bfccad41c2a25d52fbef2fa877f
Jack McMillan <jackmcmillan1249@icloud.com>
2021-05-27 22:32:45 -0500

    Fixed js

--- javascript/nav.js
+++ javascript/nav.js
@@ -10,7 +10,7 @@ const navSlide = () => {
 
   //animating the links
   navLinks.forEach((link, index) => {
-    link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 +0.3}s';
+    link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.3}s`;
   });
 }
 
