76450009dd39f59f205eacc282b282296f58e165
Jack McMillan <jackmcmillan1249@icloud.com>
2021-06-06 16:17:33 -0500

    Contact finished other then media query

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,3 +1,23 @@
+const headerElement = document.getElementsByTagName("header")[0];
+headerElement.innerHTML = `
+            <nav>
+            <div class="main-title">
+                <h4>MWIS</h4>
+            </div>
+            <ul class="links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Contracting</a></li>
+                <li><a href="contact-us.html">Contact Us</a></li>
+            </ul>
+            <div class="options-selector">
+                <div class="line-1"></div>
+                <div class="line-2"></div>
+                <div class="line-3"></div>
+            </div>
+            </nav>`;
+
+// Followed video on youtube https://www.youtube.com/watch?v=gXkqy0b4M5g changed things as nessecary
 const navSlide = () => {
   const optionsSelector = document.querySelector(".options-selector");
   const nav = document.querySelector(".links");
